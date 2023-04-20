362d571cc3e77ca0b0125d663a349af36d483791
Daniel Ufeli <danielufeli@yahoo.com>
2023-04-20 23:30:29 +0100

    test

--- src/components/customers/allcustomers.js
+++ src/components/customers/allcustomers.js
@@ -63,7 +63,7 @@ const AllCustomers = () => {
   ];
 
   const filteredCustomers = customers?.filter((customer) =>
-    customer.FirstName.toLowerCase().includes(searchText.toLowerCase())
+    customer?.FirstName.toLowerCase().includes(searchText.toLowerCase())
   );
 
   return (

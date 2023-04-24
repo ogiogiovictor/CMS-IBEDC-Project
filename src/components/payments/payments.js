cc3d7cec10ff2d61b6586b859d15069a75db0a50
OgiogioVictor <ogiogiovictor@gmail.com>
2023-04-24 01:59:15 -0600

    added new payment information

--- src/components/payments/payments.js
+++ src/components/payments/payments.js
@@ -1,15 +1,65 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { useGetAllPaymentQuery } from '../../redux/services/payment/paymentService';
+import { setPayment, setDataPayment } from './paymentSlice';
 import PaymentCard from './paymentcard';
+import PageLoader from "../spinner/loader";
+import DataTable from "../datatable";
 
 const Payments = () => {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const { payment, paymentData } = useSelector((state) => state.payment) || [];
+  const dispatch = useDispatch();
+
+  const { data, isFetching, isUninitialized, refetch } = useGetAllPaymentQuery(
+    { pageNo: currentPage });
+
+    const navigate = useNavigate();
+
+  useEffect(() => {
+    if (currentPage && data) {
+      refetch();
+      dispatch(setPayment(data?.data));
+      dispatch(setDataPayment(data?.data?.payments?.data));
+    }
+  }, [data, dispatch, currentPage, refetch]);
+
+  // console.log(payment);
+
+  const handleActionClick = ({ paymentID }) => {
+    navigate(`payment`);
+    window.scrollTo(0, 0);
+  };
+
+  const columns = [
+    { title: "Pay Date", field: "TransactionDateTime" },
+    { title: "TransID", field: "TransactionNo" },
+    { title: "AccountNo", field: "AccountNo" },
+    { title: "Meter No", field: "MeterNo" },
+    { title: "BusinessUnit", field: "BUID" },
+    { title: "RefID", field: "Token" },
+    { title: "Amount", field: "Amount" },
+  ];
+
+
     return (
         <Fragment>
-            <PaymentCard />
+            <PaymentCard payCard={payment} />
+
+            {isUninitialized ? <PageLoader /> : ''}
+
+          {isFetching ? <PageLoader /> : 
+
         <div className="row">
        <div className="col-md-12 grid-margin grid-margin-md-0 stretch-card">
          <div className="card">
            <div className="card-body">
-             <h4 className="card-title">All Payment</h4>
+             <h4 className="card-title">All Payment
+             &nbsp;&nbsp;
+             <button class="btn btn-icons btn-rounded btn-secondary" onClick={() => refetch()}><span class="icon-refresh"></span></button>
+             </h4>
            
              <div class="form-group d-flex">
                           <input type="text" class="form-control" placeholder="Search Payments(s)..." />
@@ -17,86 +67,27 @@ const Payments = () => {
                     </div>
 
              <div className="table-responsive">
-               <table className="table">
-                 <thead>
-                   <tr>
-                    <th>Transaction No</th>
-                     <th>Account Number</th>
-                     <th>Meter Number</th>
-                     <th>Customer Name</th>
-                     <th>Business Hub</th>
-                     <th>Payment Type</th>
-                     <th>Status</th>
-                   </tr>
-                 </thead>
-                 <tbody>
-                   <tr>
-                     <td>Jacob</td>
-                     <td>53275531</td>
-                     <td>12 May 2017</td>
-                     <td>Jacob</td>
-                     <td>53275531</td>
-                     <td>12 May 2017</td>
-                     <td><label className="badge badge-danger">Active</label></td>
-                   </tr>
-                   <tr>
-                     <td>Jacob</td>
-                     <td>53275531</td>
-                     <td>12 May 2017</td>
-                     <td>Jacob</td>
-                     <td>53275531</td>
-                     <td>12 May 2017</td>
-                     <td><label className="badge badge-warning">Suspended</label></td>
-                   </tr>
-                   <tr>
-                     <td>Jacob</td>
-                     <td>53275531</td>
-                     <td>12 May 2017</td>
-                     <td>Jacob</td>
-                     <td>53275531</td>
-                     <td>12 May 2017</td>
-                     <td><label className="badge badge-info">Inactive</label></td>
-                   </tr>
-                   <tr>
-                     <td>Jacob</td>
-                     <td>53275531</td>
-                     <td>12 May 2017</td>
-                     <td>Jacob</td>
-                     <td>53275531</td>
-                     <td>12 May 2017</td>
-                     <td><label className="badge badge-success">Active</label></td>
-                   </tr>
-                   <tr>
-                     <td>Jacob</td>
-                     <td>53275531</td>
-                     <td>12 May 2017</td>
-                     <td>Jacob</td>
-                     <td>53275531</td>
-                     <td>12 May 2017</td>
-                     <td><label className="badge badge-warning">Closed</label></td>
-                   </tr>
-                 </tbody>
-               </table>
+               
+             <DataTable 
+                    data={paymentData}
+                    columns={columns}
+                    pagination
+                    currentPage={currentPage}
+                    totalCount={data?.data?.payments?.total || 1}
+                    pageSize={data?.data?.payments?.per_page || 1}
+                    onPageChange={(page) => setCurrentPage(page)}
+                    onActionClick={handleActionClick}
+                    />
+
              </div>
            </div>
 
-           <div class="col-md-12">
-                <nav>
-                    <ul class="pagination rounded-flat pagination-success">
-                      <li class="page-item"><a class="page-link" href="#"><i class="icon-arrow-left"></i></a></li>
-                      <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
-                      <li class="page-item"><a class="page-link" href="#">4</a></li>
-                      <li class="page-item"><a class="page-link" href="#"><i class="icon-arrow-right"></i></a></li>
-                    </ul>
-                  </nav>
-            </div>
 
          </div>
        </div>
        
      </div>
+      }
    </Fragment>
     );
 }
